75a4614f2d530804e73f7e957fc6445d2f6e4731
Scott <scottobrien93@outlook.com>
2024-10-14 15:51:03 +0100

    Implement getApiEndpoints controller to read and return API endpoints from endpoints.json

---
+++ __tests__/api-controller.test.js
@@ -0,0 +1,31 @@
+const request = require("supertest");
+const app = require("../app.js");
+const db = require("../db/connection.js");
+const seed = require("../db/seeds/seed.js");
+const data = require("../db/data/test-data/index.js");
+
+beforeEach(() => seed(data));
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET /api", () => {
+  test("should respond with a 200 status and available endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty("GET /api/topics");
+      });
+  });
+
+  test("should respond with a 404 status for invalid routes", () => {
+    return request(app)
+      .get("/api/doesnotexist")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("Route not found");
+      });
+  });
+});
