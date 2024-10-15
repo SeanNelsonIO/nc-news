ae603ffa0b651c95a5fe5a65e53be128dcd529da
Scott <scottobrien93@outlook.com>
2024-10-15 15:24:46 +0100

    Refactored getArticleById to simplify and ensure proper error handling with catch(next)

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -7,7 +7,7 @@ const {
 exports.getArticles = (req, res, next) => {
   fetchArticles()
     .then((articles) => {
-      res.status(200).json(articles);
+      res.status(200).send(articles);
     })
     .catch((err) => {
       next(err);
@@ -16,26 +16,11 @@ exports.getArticles = (req, res, next) => {
 
 // getArticlesByID()
 exports.getArticleById = (req, res, next) => {
-  const { article_id } = req.params; // get article_id from req.params
+  const { article_id } = req.params;
 
-  // check if article_id is a valid number
-  if (isNaN(Number(article_id))) {
-    const error = new Error("Invalid article ID");
-    error.statusCode = 400;
-    return next(error);
-  }
-
-  // if valid, proceed with fetching the article (also return error if not found)
   fetchArticleById(article_id)
     .then((article) => {
-      if (!article) {
-        const error = new Error("Article not found");
-        error.statusCode = 404;
-        throw error;
-      }
-      res.status(200).json({ article });
+      res.status(200).send({ article });
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch(next);
 };
